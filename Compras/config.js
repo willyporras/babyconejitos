e6a81dfd45b2664845f2e03a1a0a2d534198943a
willyporras <willy.porras@outlook.com>
2026-09-08 00:08:34 -0500

    Actualizacion de imagenes

--- Compras/config.js
+++ Compras/config.js
@@ -1,4 +1,5 @@
-// Mismo patrón que Stock: la URL del endpoint se configura fuera de app.js.
+// Compras: historial desde su propio endpoint + catálogo/imágenes desde Stock.
 window.COMPRAS_CONFIG = {
-  apiUrl: 'https://script.google.com/macros/s/AKfycbxGTHODdbZZCn1-A6gT_eeDfpy2fW3wTUHPJ7-H-w5cxmQuEZcWFRrXmYhvoaGPVSsW/exec'
+  apiUrl: 'https://script.google.com/macros/s/AKfycbxGTHODdbZZCn1-A6gT_eeDfpy2fW3wTUHPJ7-H-w5cxmQuEZcWFRrXmYhvoaGPVSsW/exec',
+  stockApiUrl: 'https://script.google.com/macros/s/AKfycbymQFo4FQRrz1G7qi9dNPfa8J3gzgoQPWF2UAkZTKxXd8E9qir-O2x8WtlC8kyadvfP/exec'
 };
